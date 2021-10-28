8b35eec149730e0345ad4dba05ddf009de93c9cd
hanalem <bitewhanna@gmail.com>
2021-10-28 15:29:16 -0400

    commit

--- pro han/src/pachan/Myhelloclass.java
+++ pro han/src/pachan/Myhelloclass.java
@@ -5,7 +5,7 @@ public class Myhelloclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Helloooo");
+		System.out.println("Helloooo from me");
 	}
 
 }
